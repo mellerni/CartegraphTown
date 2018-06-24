162543159ac916a5c95629102506ae0bc70b7eb0
nmeller <nmeller@bridgetree.com>
2018-06-23 23:25:19 -0500

    Add Citizen and Citizen Config

---
+++ CartegraphTown/CartegraphTown.Model/Entity/Citizen.cs
@@ -0,0 +1,69 @@
+﻿namespace CartegraphTown.Model.Entity
+{
+    using System.Collections.Generic;
+    using Base.Implementations;
+
+    public class Citizen : TrackedEntity<int> 
+    {
+        // required by EF
+        protected Citizen()
+        {
+            this.Issues = new HashSet<Issue>();
+        }
+
+        /// <summary>
+        /// Citizen first name
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Citizen last name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Citizen email address
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Citizen phone number
+        /// </summary>
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// Foreign key of citizen's location
+        /// </summary>
+        public int LocationId { get; set; }
+
+
+        /// <summary>
+        /// Current physical address of citizen
+        /// </summary>
+        public virtual Location Location { get; set; }
+
+        /// <summary>
+        /// Collection of child locations
+        /// </summary>
+        public virtual ICollection<Issue> Issues { get; set; }
+
+        /// <summary>
+        /// Citizen full name
+        /// </summary>
+        public string FullName => this.LastName + ", " + this.FirstName;
+
+        /// <summary>
+        /// Citizen type ahead
+        /// </summary>
+        public string GetTypeAhead()
+        {
+            var typeAhead = "Name: " + this.FullName;
+            if (this.Location?.Address1 != null)
+            {
+                typeAhead += " - Address: " + this.Location.Address1;
+            }
+
+            return typeAhead;
+        }
+    }
+}
---
+++ CartegraphTown/CartegraphTown.Model/Entity/Configuration/CitizenConfig.cs
@@ -0,0 +1,58 @@
+﻿namespace CartegraphTown.Model.Entity.Configuration
+{
+    using System.Data.Entity.ModelConfiguration;
+
+    internal class CitizenConfig : EntityTypeConfiguration<Citizen>
+    {
+        internal CitizenConfig()
+        {
+            this.HasKey(e => e.Id); ;
+
+            this.ToTable("Citizens");
+
+            // Ignored Properties
+            this.Ignore(x => x.FullName);
+
+            this.Property(e => e.FirstName)
+                .HasColumnName("FirstName")
+                .HasMaxLength(250)
+                .IsUnicode(false);
+
+            this.Property(e => e.LastName)
+                .HasColumnName("LastName")
+                .HasMaxLength(250)
+                .IsUnicode(false);
+
+            this.Property(e => e.Email)
+                .HasColumnName("Email")
+                .HasMaxLength(250)
+                .IsUnicode(false);
+
+            this.Property(e => e.Phone)
+                .HasColumnName("Phone")
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+            this.Property(e => e.LocationId)
+                .HasColumnName("LocationId");
+
+            this.Property(e => e.CreatedDate)
+                .HasColumnName("CreatedDate")
+                .IsRequired();
+
+            this.Property(e => e.CreatedBy)
+                .HasColumnName("CreatedBy")
+                .IsRequired();
+
+            this.Property(e => e.UpdatedDate)
+                .HasColumnName("UpdatedDate");
+
+            this.Property(e => e.UpdatedBy)
+                .HasColumnName("UpdatedBy");
+
+            this.HasRequired(e => e.Location)
+                .WithMany(x => x.Citizens)
+                .HasForeignKey(e => e.LocationId);
+        }
+    }
+}
