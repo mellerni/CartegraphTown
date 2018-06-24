089325ed3425a216a0849a875b9b4d524a4e48eb
nmeller <nmeller@bridgetree.com>
2018-06-23 23:22:39 -0500

    Add Citizens table

---
+++ CartegraphTown/CartegraphTown.Database/dbo/Tables/Citizens.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Citizens] (
+    [Id]                                INT IDENTITY (1, 1) NOT NULL,
+    [FirstName]                         NVARCHAR (250)      NOT NULL,
+    [LastName]                          NVARCHAR (250)      NOT NULL,
+    [Email]                             NVARCHAR (250)      NULL,   
+    [Phone]                             NVARCHAR (50)       NULL,   
+    [LocationId]                        INT                 NOT NULL,  
+    [CreatedDate]                       DATETIME            NOT NULL,
+    [CreatedBy]                         INT                 NOT NULL,
+    [UpdatedDate]                       DATETIME            NULL,
+    [UpdatedBy]                         INT                 NULL,
+    CONSTRAINT [PK_Citizens] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Citizen_Location] FOREIGN KEY ([LocationId]) REFERENCES [dbo].[Locations] ([Id])
+);
+
