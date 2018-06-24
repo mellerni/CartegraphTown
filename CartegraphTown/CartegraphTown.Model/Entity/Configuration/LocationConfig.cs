452daca41365681831990a6e75fc8d2f28d9250d
nmeller <nmeller@bridgetree.com>
2018-06-23 23:27:08 -0500

    Add Location and Location Config

---
+++ CartegraphTown/CartegraphTown.Model/Entity/Configuration/LocationConfig.cs
@@ -0,0 +1,58 @@
+﻿namespace CartegraphTown.Model.Entity.Configuration
+{
+    using System.Data.Entity.ModelConfiguration;
+
+    internal class LocationConfig : EntityTypeConfiguration<Location>
+    {
+        internal LocationConfig()
+        {
+            this.HasKey(e => e.Id); ;
+
+            this.ToTable("Locations");
+
+            this.Property(e => e.Address1)
+                .HasColumnName("Address1")
+                .HasMaxLength(250)
+                .IsUnicode(false);
+
+            this.Property(e => e.Address2)
+                .HasColumnName("Address2")
+                .HasMaxLength(250)
+                .IsUnicode(false);
+
+            this.Property(e => e.City)
+                .HasColumnName("City")
+                .HasMaxLength(250)
+                .IsUnicode(false);
+
+            this.Property(e => e.ZipCode)
+                .HasColumnName("ZipCode")
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+            this.Property(e => e.Latitude)
+                .HasColumnName("Latitude");
+
+            this.Property(e => e.Longitude)
+                .HasColumnName("Longitude");
+
+            this.Property(e => e.CreatedDate)
+                .HasColumnName("CreatedDate")
+                .IsRequired();
+
+            this.Property(e => e.CreatedBy)
+                .HasColumnName("CreatedBy")
+                .IsRequired();
+
+            this.Property(e => e.UpdatedDate)
+                .HasColumnName("UpdatedDate");
+
+            this.Property(e => e.UpdatedBy)
+                .HasColumnName("UpdatedBy");
+
+            this.HasOptional(e => e.State)
+                .WithMany(x => x.Locations)
+                .HasForeignKey(e => e.StateId);
+        }
+    }
+}
---
+++ CartegraphTown/CartegraphTown.Model/Entity/Location.cs
@@ -0,0 +1,66 @@
+﻿namespace CartegraphTown.Model.Entity
+{
+    using System;
+    using System.Collections.Generic;
+    using Base.Implementations;
+
+    public class Location : TrackedEntity<int>
+    {
+        // required by EF
+        protected Location()
+        {
+            this.Issues = new HashSet<Issue>();
+            this.Citizens = new HashSet<Citizen>();
+        }
+
+        /// <summary>
+        /// Address line one of location
+        /// </summary>
+        public string Address1 { get; set; }
+
+        /// <summary>
+        /// Address line two of location
+        /// </summary>
+        public string Address2 { get; set; }
+
+        /// <summary>
+        /// City of location
+        /// </summary>
+        public string City { get; set; }
+
+        /// <summary>
+        /// Foreign key of location's state
+        /// </summary>
+        public int StateId { get; set; }
+
+        /// <summary>
+        /// Zip code of location [45678 or 45678-9010] 
+        /// </summary>
+        public string ZipCode { get; set; }
+
+        /// <summary>
+        /// Latitude of location
+        /// </summary>
+        public float? Latitude { get; set; }
+
+        /// <summary>
+        /// Longitude of location
+        /// </summary>
+        public float? Longitude { get; set; }
+
+        /// <summary>
+        /// State of location
+        /// </summary>
+        public virtual State State { get; set; }
+
+        /// <summary>
+        /// Collection of issues
+        /// </summary>
+        public virtual ICollection<Issue> Issues { get; set; }
+
+        /// <summary>
+        /// Collection of citizens
+        /// </summary>
+        public virtual ICollection<Citizen> Citizens { get; set; }
+    }
+}
