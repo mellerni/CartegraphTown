f20d27c211936d56b2a76a033370635b1db0723b
nmeller <nmeller@bridgetree.com>
2018-06-23 23:22:14 -0500

    Add states table

---
+++ CartegraphTown/CartegraphTown.Database/dbo/Tables/States.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[States] (
+    [Id]                                INT IDENTITY (1, 1)     NOT NULL,
+    [Abbreviation]                      NVARCHAR (2)            NOT NULL,
+    [Name]                              NVARCHAR (100)          NOT NULL,
+    CONSTRAINT [PK_States] PRIMARY KEY ([Id])
+);
+
