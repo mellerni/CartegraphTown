259ca7094519d214663c99ed2fe4adb20a3b2aa3
nmeller <nmeller@bridgetree.com>
2018-06-23 23:26:36 -0500

    Add State and State Config

---
+++ CartegraphTown/CartegraphTown.Model/Entity/Configuration/StateConfig.cs
@@ -0,0 +1,26 @@
+﻿namespace CartegraphTown.Model.Entity.Configuration
+{
+    using System.Data.Entity.ModelConfiguration;
+
+    internal class StateConfig : EntityTypeConfiguration<State>
+    {
+        internal StateConfig()
+        {
+            this.HasKey(e => e.Id); ;
+
+            this.ToTable("States");
+
+            this.Property(e => e.Abbreviation)
+                .HasColumnName("Abbreviation")
+                .HasMaxLength(2)
+                .IsRequired()
+                .IsUnicode(false);
+
+            this.Property(e => e.Name)
+                .HasColumnName("Name")
+                .HasMaxLength(100)
+                .IsRequired()
+                .IsUnicode(false);
+        }
+    }
+}
---
+++ CartegraphTown/CartegraphTown.Model/Entity/State.cs
@@ -0,0 +1,30 @@
+﻿namespace CartegraphTown.Model.Entity
+{
+    using System.Collections.Generic;
+    using Base.Implementations;
+
+    public class State : EntityBase<int>
+    {
+        // required by EF
+        protected State()
+        {
+            this.Locations = new HashSet<Location>();
+        }
+
+        /// <summary>
+        /// State abbreviation
+        /// </summary>
+        public string Abbreviation { get; set; }
+        
+        /// <summary>
+        /// State full name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Collection of child locations
+        /// </summary>
+        public virtual ICollection<Location> Locations { get; set; }
+
+    }
+}
