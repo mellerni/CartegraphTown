872e810c2f784bad1e83f067ce0ef26f1e87a2f6
nmeller <nmeller@bridgetree.com>
2018-06-23 23:23:06 -0500

    Add Issues table

---
+++ CartegraphTown/CartegraphTown.Database/dbo/Tables/Issues.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Issues] (
+    [Id]                                INT IDENTITY (1, 1) NOT NULL,
+    [IssueType]                         INT                 NOT NULL,
+    [LocationId]                        INT                 NOT NULL,
+    [CitizenId]                         INT                 NOT NULL,
+    [Details]                           NVARCHAR (4000)     NULL,
+    [CreatedDate]                       DATETIME            NOT NULL,
+    [CreatedBy]                         INT                 NOT NULL,
+    [UpdatedDate]                       DATETIME            NULL,
+    [UpdatedBy]                         INT                 NULL,
+    [CorrectiveAction]                  NVARCHAR (4000)     NULL,
+    [CorrectionDate]                    DATETIME            NULL,
+    CONSTRAINT [PK_Issues] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Issue_Citizen] FOREIGN KEY ([CitizenId]) REFERENCES [dbo].[Citizens] ([Id]),
+    CONSTRAINT [FK_Issue_Location] FOREIGN KEY ([LocationId]) REFERENCES [dbo].[Locations] ([Id])
+);
+
