8ae0d9fc00ee445d7a350b435c87b35da5d3e298
nmeller <nmeller@bridgetree.com>
2018-06-24 20:18:25 -0500

    Add Serilog exception table

---
+++ CartegraphTown/CartegraphTown.Database/dbo/Tables/ExceptionLogs.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[ExceptionLogs] (
+    [Id]              INT                IDENTITY (1, 1) NOT NULL,
+    [Message]         NVARCHAR (MAX)     NULL,
+    [MessageTemplate] NVARCHAR (MAX)     NULL,
+    [Level]           NVARCHAR (128)     NULL,
+    [TimeStamp]       DATETIMEOFFSET (7) NOT NULL,
+    [Exception]       NVARCHAR (MAX)     NULL,
+    [Properties]      XML                NULL,
+    [LogEvent]        NVARCHAR (MAX)     NULL,
+    CONSTRAINT [PK_ExceptionLogs] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
