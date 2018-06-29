a12e3eda5ef4a95ea934a588b31be64864180e89
nmeller <nmeller@bridgetree.com>
2018-06-28 22:16:37 -0500

    add connection test to navmenu.component

--- CartegraphTown/CartegraphTown.Web/ClientApp/app/components/navmenu/navmenu.component.ts
+++ CartegraphTown/CartegraphTown.Web/ClientApp/app/components/navmenu/navmenu.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import { State } from '../../classes/state';
+import { LocationService } from '../../services/location.service';
 
 @Component({
     selector: 'nav-menu',
@@ -6,4 +8,34 @@ import { Component } from '@angular/core';
     styleUrls: ['./navmenu.component.css']
 })
 export class NavMenuComponent {
+    public loading: boolean = true;
+    public showStatus: boolean = true;
+    public isConnected: boolean = false;
+    public hasLostConnection: boolean = false;
+    public states: State[] | undefined;
+
+    constructor(private locationService: LocationService) {
+    }
+
+    ngOnInit() {
+        this.getStates();
+    }
+
+    getStates()
+    {
+        this.loading = false;
+        this.locationService.getStates()
+        .then(result => {
+            this.states = result as State[];
+            this.loading = false;
+            this.isConnected = true;
+            setTimeout(() => {
+                this.showStatus = false;
+            }, 10000);
+        })
+        .catch(error => {
+            this.loading = false;
+            this.hasLostConnection = true;
+        });
+    }
 }
